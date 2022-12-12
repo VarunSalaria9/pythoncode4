241f8e373cb3b969dda1d9c88cc26f80f308a312
VarunSalaria12219988 <119073114+VarunSalaria9@users.noreply.github.com>
2022-12-12 21:29:10 +0530

    Add files via upload

---
+++ lecture11.py
@@ -0,0 +1,14 @@
+#comments
+#escape sequence
+print("hello \"world\" world")
+print('I\'m Sagar')
+
+print("hello \"world\" world")#\n=next line
+
+
+print("name\tSagar")#\t=tab
+
+print("this is backslash \\") #\\ is for single backslash
+print("this is double backslash \\\\")#\\\\ is for double backslash
+
+print("hell\blo")   #\b=remove letter
